71d59bff3b0634945742b713e07c7dc4f765d6a3
GlerionSH <rubentoramirez@gmail.com>
2025-03-06 10:38:08 +0100

    Cambio Contacto.java

--- Contacto.java
+++ Contacto.java
@@ -7,7 +7,7 @@ public class Contacto {
        private Direccion direccion;
       /**
         * Constructor de la clase Contacto. 
-        * @param direccion Dirección del contacto.
+        * @param direccion Dirección del contacto
         * @param nombre Nombre del contacto.
         * @param telefono Teléfono del contacto.
         */
